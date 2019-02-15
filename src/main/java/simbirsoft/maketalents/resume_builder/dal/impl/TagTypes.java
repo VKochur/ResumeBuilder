6348184ba029ce5818fd6b2856ceeeb73ee05b32
kvv <v.kochurov@rambler.ru>
2019-02-15 15:15:22 +0400

    adding packages structure

---
+++ src/main/java/simbirsoft/maketalents/resume_builder/dal/impl/TagTypes.java
@@ -0,0 +1,15 @@
+package main.java.simbirsoft.maketalents.resume_builder.dal.impl;
+
+public enum TagTypes {
+    FIO,
+    DOB,
+    EMAIL,
+    PHONE,
+    SKYPE,
+    URL_AVATAR,
+    TARGET,
+    EXPERIENCE,
+    BD_EDUCATION,
+    AD_EDUCATION,
+    OTHER_INFO
+}
---
+++ src/main/java/simbirsoft/maketalents/resume_builder/util/Logger.java
@@ -0,0 +1,9 @@
+package main.java.simbirsoft.maketalents.resume_builder.util;
+
+public class Logger {
+
+    //TODO: переписать вывод об ошибках в файл. Создавать файл заново при перезапуске программы
+    public static void processingEx(Exception e){
+        System.out.println(e);
+    }
+}
